68b78e2a59081e7223bb46e1402351635afbe0df
Denis Ouma <53114809+DenisOuma@users.noreply.github.com>
2021-06-24 14:48:58 -0400

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,19 +5,9 @@
  */
 void print_numbers(void)
 {
-<<<<<<< HEAD
 	int i;
 
 	for (i = 0; i < 10; i++)
 		_putchar(i + 48);
 	_putchar('\n');
-=======
-int i = 0;
-while (i < 10)
-{
-_putchar(i);
-i++
-}
-_putchar('\n');
->>>>>>> abf1bf607fcd8b0ef7522a12e7e8a4547b774afc
 }
