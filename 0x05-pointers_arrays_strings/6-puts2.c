ee5c5c3b2415abaefe38a2a1ca3205b8e85ed2b3
DenisOuma <dennisouma38@gmail.com>
2021-06-28 16:09:10 -0400

    half a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,17 +3,17 @@
 /**
  * puts2 - prints every other character of a  string to stdout
  * @str: string to print
- *
  * Return: void
  */
 void puts2(char *str)
 {
-        int i = 0;
+	int i = 0;
 
-        while (*(str + i))
-        {
-                _putchar(*(str + i));
-                i = i + 2;
-        }
-        _putchar('\n');
+	while (*(str + i))
+	{
+		 _putchar(*(str + i));
+		i = i + 2;
+	}
+	_putchar('\n');
+
 }
