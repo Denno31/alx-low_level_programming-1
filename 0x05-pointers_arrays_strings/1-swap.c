a0c9846dac5e55f8f3dbd7f6fc80ed0b4c487c6d
DenisOuma <dennisouma38@gmail.com>
2021-06-28 13:56:10 -0400

    Swap Values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * swap_int - check the code for ALX School students.
+ *@a: first value to be swaped
+ *@b: second value to be swaped
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int a = 42;
+	int b = 98;
+
+	swap(&a, &b);
+}
