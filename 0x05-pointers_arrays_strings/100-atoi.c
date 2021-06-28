585f840787730f285d0d2adb43167d75b43c20cc
DenisOuma <dennisouma38@gmail.com>
2021-06-28 16:35:07 -0400

    changes

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,26 +1,28 @@
 #include "holberton.h"
+
 /**
- * _atoi - copies a string
- * @s: source to copy string to
+ * _atoi - converts a string to an integer
+ * @s: string to convert
  *
- * Return: copied string
+ * Return: value of integer
  */
 int _atoi(char *s)
 {
-	int i, j, k = 0;
+	int i, j, n, m;
 
-	for ( i = 0 ; s[i] != 0 ; i++ )
-		{
-			if (s[i] == '-')
-			j++;
-			if ((s[i] >= '0') && (s[i] <= '9'))
-			{
-				s[k] = s[i];
-				k++;
-			}
-		}
-		if (j % 2 == 0)
-		return (s)
-		else
-		return (-s);
+	i = n = 0;
+	m = 1;
+	while ((*(s + i) < '0' || *(s + i) > '9') && (*(s + i) != '\0'))
+	{
+		if (*(s + i) == '-')
+			m *= -1;
+		i++;
+	}
+	j = i;
+	while ((*(s + j) >= '0') && (*(s + j) <= '9'))
+	{
+		n = n * 10 + m * (*(s + j) - '0');
+		j++;
+	}
+	return (n);
 }
