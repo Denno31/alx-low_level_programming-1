b1729ab199de202d5340ec23d1fd46a04fc2fb26
DenisOuma <dennisouma38@gmail.com>
2021-07-12 20:20:47 +0300

    addition of positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - arguments to main
+ * @argc: first arg is a count
+ * @argv: second arg is a pointer to char
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int x, y;
+	int add = 0;
+
+	x = 1;
+	while (argv[x])
+	{
+		y = 1;
+		while (argv[x][y])
+		{
+			if ((isdigit(argv[x][y]) == 0))
+			{
+				printf("error\n");
+				return (1);
+			}
+		y++;
+		}
+	x++;
+	}
+	x = 1;
+
+	while (argv[i] != argv[argc])
+	{
+		add += atoi(argv[x]);
+		x++;
+	}
+
+	printf("%d\n", add);
+return (0);
+}
